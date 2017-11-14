32ae7d3e489d44acddb6bdc271fc1c8a9d1dc613
Camilo Higuita <milo.h@aol.com>
2017-11-14 09:12:52 -0500

    ajax

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require_once 'crud.php';
 
 
 
-if ( isset($_POST["create"]) ){
+
 if ( isset($_POST["id"]) && !empty($_POST["id"]) &&
 isset($_POST["name"]) && !empty($_POST["name"]) && 
 isset($_POST["lastname"]) && !empty($_POST["lastname"]) && isset($_POST["phone"]) && !empty($_POST["phone"]) )
@@ -20,13 +20,11 @@ $con->phone = $_POST["phone"];
 
 $crud->create($con);
 
-echo ("<h1>".$con."</h1>");
-}
-}else {
-    
-    echo ("<h1> it's not submit </h1>");
+echo true;
 
-}
+}else echo false;
+
+
 
 
 
--- view.php
+++ view.php
@@ -3,6 +3,9 @@
 <head>
 
 <link></link>
+
+<script type="text/javascript" src="jquery-3.2.1.min.js"> </script>
+
 <style>
 
 form input
@@ -22,24 +25,60 @@ form input
 
 
 <h2>Create contact</h2>
-<form method="post" action="main.php">
-<input type="text" placeholder="id" name="id" required></input>
-<input type="text" placeholder="name" name="name" required></input>
-<input type="text" placeholder="lastname" name="lastname" required></input>
-<input type="text" placeholder="phone" name="phone"  required></input>
-<input type="submit" value="create" name="create"></input>
+<form method="post" action="#">
+<input type="text" placeholder="id" id="id" required></input>
+<input type="text" placeholder="name" id="name" required></input>
+<input type="text" placeholder="lastname" id="lastname" required></input>
+<input type="text" placeholder="phone" id="phone"  required></input>
+<input type="button" value="create" id="create" onClick="javascript:sendData();"></input>
 </form>
 
 <h2>Search</h2>
-<form method="post" action="main.php">
-<input type="text" placeholder="query" name="query"  required></input>
-<input type="submit" value="search" name="search"></input>
+<form method="post" action="#">
+<input type="text" placeholder="query" id="query"  required></input>
+<input type="button" value="search" id="search"></input>
 </form>
 
 <div>
 <h2>Output</h2>
 <label id="output"> </label>
 </div>
+
+<script>
+
+function sendData()
+{
+    
+    var id = document.getElementById('id')
+    var name = document.getElementById('name')
+    var lastname = document.getElementById('lastname')
+    var phone = document.getElementById('phone')
+    
+    $.ajax({
+        
+        type: 'POST',
+        url: 'main.php',
+        data: ("id="+id+"&name="+name+"&lastname="+lastname+"&phone="+phone),
+        success: function(response)
+        {
+            if(response)
+            {
+                $("#output").html(response)            
+            
+            }else $("#output").html("Contact couldn't be saved")              
+        }  
+    
+    
+    })
+    
+
+
+}
+
+
+
+</script>
+
 
 </body>
 
